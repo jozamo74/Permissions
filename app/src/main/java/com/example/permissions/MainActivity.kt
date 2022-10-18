34341123741e50eb3adee2fa20edb689aa438f10
José Zambrano <zambranomoya74@gmail.com>
2022-10-18 20:24:06 +0200

    02-Permission

--- app/src/main/java/com/example/permissions/MainActivity.kt
+++ app/src/main/java/com/example/permissions/MainActivity.kt
@@ -4,23 +4,25 @@ package com.example.permissions
 import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.*
 import com.example.permissions.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    val coarsePermission =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted ->
-            when {
-                isGranted -> toast ("Granted")
-                shouldShowRequestPermissionRationale(ACCESS_COARSE_LOCATION) ->
-                    toast("Show rationale")
-                else ->toast("Denied")
-            }
 
-        }
+
+    private val coarsePermission = PermissionRequester(
+        this,
+        ACCESS_COARSE_LOCATION,
+        onRationale = { toast("Show Rationale")},
+        onDenied = { toast("Denied")}
+
+    )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +30,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnPermissions.setOnClickListener {
-           coarsePermission.launch(ACCESS_COARSE_LOCATION)
+           coarsePermission.runWithPermission {
+                toast("Granted")
+           }
         }
 
     }
 }
+
+class PermissionRequester(
+    activity: ComponentActivity,
+    private val permission: String,
+    private val onRationale: () -> Unit = {},
+    private val onDenied: () -> Unit = {}
+    ) {
+
+    private var onGranted: () -> Unit = {}
+    private val permissionLauncher =
+        activity.registerForActivityResult(RequestPermission()){ isGranted ->
+        when {
+            isGranted -> onGranted()
+            activity.shouldShowRequestPermissionRationale(permission) -> onRationale()
+            else -> onDenied()
+        }
+
+    }
+
+    fun runWithPermission(body: () -> Unit) {
+        onGranted = body
+        permissionLauncher.launch(permission)
+
+    }
+
+}
+
